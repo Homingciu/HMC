d7f7772ef0c015eb4979edaea784fac26bb97955
HMC <1154611849@qq.com>
2017-03-29 19:34:10 +0800

    crash

--- src/js/index.js
+++ src/js/index.js
@@ -23,7 +23,7 @@ var MyPlane = React.createClass({
     },
     render: function () {
         return (
-            <div ref="myPlane" style={this.props.style} id="myPlane" onTouchStart={this.props.drag}></div>
+            <div ref="myPlane" style={this.props.style} id="myPlane" className="myPlane" onTouchStart={this.props.drag}></div>
         )
     }
 })
@@ -124,14 +124,14 @@ var App = React.createClass({
         plane.addEventListener("touchend", clear);
 
 
-        //触发点不对，不应该由touchStart来触发
-        function updataBullet() {
-            _selft.setState({
-                 move: true
-            })
-            requestAnimationFrame(updataBullet);
-        }
-        requestAnimationFrame(updataBullet);
+        // //触发点不对，不应该由touchStart来触发
+        // function updataBullet() {
+        //     _selft.setState({
+        //          move: true
+        //     })
+        //     requestAnimationFrame(updataBullet);
+        // }
+        // requestAnimationFrame(updataBullet);
 
         function clear() {
             plane.removeEventListener("touchmove", move);
@@ -141,6 +141,18 @@ var App = React.createClass({
         function move(event) {
             plane.style.left = event.touches[0].clientX - disX + "px";
             plane.style.top = event.touches[0].clientY - disY + "px";
+            if(plane.offsetLeft >= window.innerWidth - plane.offsetWidth) {
+                plane.style.left = window.innerWidth - plane.offsetWidth / 2 + "px";
+            }else if(plane.offsetLeft <= 0) {
+                plane.style.left = 0 + plane.offsetWidth / 2 + "px";
+            }
+            
+            if(plane.offsetTop >= window.innerHeight - plane.offsetHeight) {
+                plane.style.top = window.innerHeight - plane.offsetHeight + "px";
+            }else if(plane.offsetTop <= 0) {
+                plane.style.top = 0;
+            }
+
             event.preventDefault(); //防止屏幕滑动
         }
     },
@@ -191,12 +203,27 @@ requestAnimationFrame(add);
 function planeMove(ele) {
     var speed = 2;
     function move() {
+        //判断有没有中子弹
+        if(!(ele.num >= 0)) {
+            ele.num = 0;
+        }
+        var $Bullet = document.getElementsByClassName("bullet"),
+            len = $Bullet.length,
+            i;
+        var myPlane = document.getElementById("myPlane");    
+        for(i = 0; i < len; i ++) {
+            isCrash(ele, $Bullet[i]);
+        }
+        isCrash(ele, myPlane);
+
+        //---------------------------移动------------------------------
         ele.style.top = ele.offsetTop + speed + "px";
         if(ele.offsetTop <= window.innerHeight) {
             requestAnimationFrame(move);
         }
         
     }
+    
     requestAnimationFrame(move);
 }
 
@@ -206,7 +233,7 @@ var planeNumber = 1;
 setInterval(function () {
     var planeArry = document.getElementsByClassName("plane");
     if(planeNumber < 5) {
-        planeNumber = Math.floor(lastTime / 1000);
+        planeNumber = Math.floor(lastTime / 10000) || 1;
     }
     if(planeArry.length < planeNumber) {
         var kind = Math.ceil(Math.random() * 6);  //1~6
@@ -241,7 +268,7 @@ setInterval(function () {
 
 //使子弹移动
 function bulletMove(ele) {
-    var speed = 18;
+    var speed = 10;
     function move() {
         ele.style.top = ele.offsetTop - speed + "px";
         requestAnimationFrame(move);
@@ -254,7 +281,7 @@ var myPlane = $("#myPlane")[0];
 function addBullet() {
     $("<div>")
             .addClass("bullet")
-            .css({left: myPlane.offsetLeft + myPlane.offsetWidth / 2 - 5, top: myPlane.offsetTop - 20})
+            .css({left: myPlane.offsetLeft + myPlane.offsetWidth / 2 - 5, top: myPlane.offsetTop})
             .appendTo($("#bulletBox"))
     var bulletArry = document.getElementsByClassName("bullet");
     bulletMove(bulletArry[bulletArry.length - 1]);     
@@ -269,7 +296,7 @@ function addBullet() {
     }
 }
 
-setInterval(addBullet, 200)
+var bulletTimer = setInterval(addBullet, 200);
 
 //------------------------------------------道具-------------------------
 
@@ -292,10 +319,18 @@ function addItem() {
     var item  = document.getElementsByClassName("item");
     if(item) {
         if(item.length < 1) {
-            $("<div>")
-                    .addClass("item")
-                    .css({left: Math.floor(Math.random() * (window.innerWidth - 30)) +"px"})
-                    .appendTo($("#itemBox"))
+            var div = $("<div>");
+            div
+                .addClass("item")
+                .css({left: Math.floor(Math.random() * (window.innerWidth - 30)) +"px"})
+                .appendTo($("#itemBox"))
+
+            //随机出现2种不同的道具    
+            if(Math.random() * 10 > 5) {
+                div.addClass("buffBullet");
+            }else{
+                div.addClass("boom")
+            }        
             
             itemMove(item[item.length - 1]); 
         }
@@ -308,3 +343,106 @@ function addItem() {
        
 }
 setInterval(addItem, 1000)
+
+
+
+
+//吃道具之后触发一个函数。
+//1.使子弹变成2个
+var doubleBulletTimer;
+function doubleBullet() {
+    //左边子弹
+    $("<div>")
+            .addClass("bullet doubleBullet")
+            .css({left: myPlane.offsetLeft + myPlane.offsetWidth / 2 - 20, top: myPlane.offsetTop})
+            .appendTo($("#bulletBox"))
+    //右边子弹        
+    $("<div>")
+            .addClass("bullet doubleBullet")
+            .css({left: myPlane.offsetLeft + myPlane.offsetWidth / 2 + 10, top: myPlane.offsetTop})
+            .appendTo($("#bulletBox"))        
+    var bulletArry = document.getElementsByClassName("bullet");
+    bulletMove(bulletArry[bulletArry.length - 2]); 
+    bulletMove(bulletArry[bulletArry.length - 1]);     
+    var i,
+        len = bulletArry.length;
+    for(i = 0; i < len; i++) {
+        if(bulletArry[i]) {
+            if(bulletArry[i].offsetTop < 0) {
+                bulletArry[i].remove();
+            }
+        }
+    }
+    
+}
+function eatBuffBullet() {
+    clearInterval(bulletTimer);
+    doubleBulletTimer = setInterval(doubleBullet, 200);
+    setTimeout(function ()  {
+        clearInterval(doubleBulletTimer);
+        bulletTimer = setInterval(addBullet, 200);
+    }, 2000) 
+}
+
+
+//假如吃到了道具就触发eatBuffBullet函数；
+eatBuffBullet();
+
+// $("<div>").appendTo($("#test"))
+// $("<div>").appendTo($("#test"))
+
+
+//2.增加炸弹数量
+
+
+
+//用来触发炸弹
+document.onclick = function () {
+    //如果炸弹数＞1，就触发
+    $("#enemy").empty();
+}
+
+
+// //判断2个物体有没有撞到(用来判断各种碰撞，捡道具，打中飞机等)
+function isCrash(top, bottom) {
+    if(top && bottom) {
+        if (
+            (top.offsetTop > bottom.offsetTop - 10 && top.offsetLeft < bottom.offsetLeft && top.offsetLeft + top.offsetWidth > bottom.offsetLeft)
+            )  {
+                    
+                    // $(bottom).remove();
+                    top.num ++;
+                    switch(top.className) {
+                        case "smallPlane plane": 
+                            if(top.num > 1) {
+                                $(top).remove();
+                            }
+                            break;
+                        case "middlePlane plane": 
+                            if(top.num > 6) {
+                                $(top).remove();
+                            }
+                            break;
+                        case "bigPlane plane": 
+                            if(top.num > 10) {
+                                $(top).remove();
+                            }
+                            break;                
+                    }
+                    if(bottom.className == "bullet" || bottom.className == "bullet doubleBullet") {
+                        $(bottom).remove();
+                    }else if(bottom.className == "myPlane" ) {
+                        window.location.reload();   //刷新页面
+                        alert("game over");
+                    }
+            }
+    }
+}
+
+
+
+//测试碰撞
+// var test = document.getElementsByClassName("test");
+// test[1].style.top = "20px";
+// test[1].style.left = "20px";
+// isCrash(test[0], test[1]);
