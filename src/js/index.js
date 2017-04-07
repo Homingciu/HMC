3336868888cc844664cbbe66e4b42e6211cdf395
HMC <1154611849@qq.com>
2017-04-07 21:41:11 +0800

    enemy3 myplane bug

--- src/js/index.js
+++ src/js/index.js
@@ -962,6 +962,6 @@ function bigPlaneAnimation() {
 var bigPlaneTimer = setInterval(bigPlaneAnimation, 100);
 
 
-if($("#myPlane").css("backgroundImage")  == 'url("file:///C:/Users/Administrator/Desktop/web/react/src/img/hero1.png")') {
-    console.log(1);
+if($("#myPlane").css("backgroundImage")  != 'url("file:///C:/Users/Administrator/Desktop/web/react/src/img/hero1.png")') {
+    console.log($("#myPlane").css("backgroundImage"));
 }
